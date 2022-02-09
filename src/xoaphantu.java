0828c37fd992615fad0f7ff07a0a0743668b4131
unknown <luongvu2605@gmail.com>
2022-02-09 21:47:28 +0700

    xoaphantu

---
+++ src/xoaphantu.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class xoaphantu {
+    public static void main(String[] args) {
+        int[] a = {3, 2, 6, 4, 8};
+        int index;
+        int[] newArray;
+        int k=0;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Nhap phan tu ban muon xoa : ");
+        index=scanner.nextInt();
+        newArray=new int[a.length-1];
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=index){
+                a[k] = a[i];
+                k++;
+                }
+        }
+        System.out.println("Mang sau khi xoa la : ");
+        for(int i=0;i<k;i++){
+            System.out.print(a[i] + "\t");
+        }
+      }
+    }
